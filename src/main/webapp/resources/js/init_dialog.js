763c1d95a31d2a73465784db65e5030365654f7f
Skevary <skevarys@gmail.com>
2017-09-27 20:26:31 +0300

    Add animation

---
+++ src/main/webapp/resources/js/init_dialog.js
@@ -0,0 +1,6 @@
+    $('#login-modal').on('show.bs.modal', function(e) {
+            $('.modal-dialog').velocity('transition.perspectiveUpIn');
+    });
+    // $('#modal_signup').on('hide.bs.modal', function(e) {
+    //     $('.modal-dialog').velocity('transition.bounceUpIn');
+    // });
