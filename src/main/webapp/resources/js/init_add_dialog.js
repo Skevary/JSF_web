5946ad241c6972d89e880c295727297d2b21e4b4
Skevary <skevarys@gmail.com>
2017-09-28 21:16:50 +0300

    Update

--- src/main/webapp/resources/js/init_add_dialog.js
+++ src/main/webapp/resources/js/init_add_dialog.js
@@ -1,5 +1,5 @@
-$('#login-modal').on('show.bs.modal', function (e) {
-    $('.modal-dialog').velocity('transition.perspectiveDownIn');
+$('#add_new_data').on('show.bs.modal', function (e) {
+    $('.modal-dialog').velocity('transition.whirlIn');
 });
 // $('#modal_signup').on('hide.bs.modal', function(e) {
 //     $('.modal-dialog').velocity('transition.bounceUpIn');
---
+++ src/main/webapp/resources/js/validators/input_number.js
@@ -0,0 +1,16 @@
+$(function () {
+    let inputEl = document.getElementById('dialog_block:input_number');
+
+    inputEl.oninput = function (e) {
+        let inputVal = e.target.value;
+
+        //give you NaN if not a number
+        let numberVal = parseInt(inputVal);
+
+        if (!numberVal || numberVal < 0) {
+            e.target.value = 0;
+        } else if (numberVal > 200) {
+            e.target.value = 200;
+        }
+    };
+});
